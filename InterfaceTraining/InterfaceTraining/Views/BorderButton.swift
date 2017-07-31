94e4b259635ffd423d4e5f0fbb54759ec31a7a4f
Yashim Greene <ykgreene@Yashims-MacBook-Pro.local>
2017-07-30 23:00:28 -0500

    initial setup and asset inclusion

---
+++ InterfaceTraining/InterfaceTraining/Views/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  InterfaceTraining
+//
+//  Created by Yashim Greene on 7/30/17.
+//  Copyright © 2017 Yashim Greene. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
