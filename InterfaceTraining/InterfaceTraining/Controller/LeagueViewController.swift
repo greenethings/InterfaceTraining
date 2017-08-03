9b86ef6f2dec78c31d2715054e999a15d857a6f1
Yashim Greene <ykgreene@Yashims-MacBook-Pro.local>
2017-08-02 23:27:22 -0500

    created IBActions and Structs

--- InterfaceTraining/InterfaceTraining/Controller/LeagueViewController.swift
+++ InterfaceTraining/InterfaceTraining/Controller/LeagueViewController.swift
@@ -10,19 +10,40 @@ import UIKit
 
 class LeagueViewController: UIViewController {
     
+    var player: Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
+        
+        player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillViewControllerSegue", sender: self)
     }
    
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
     @IBAction func unwindFromSkillViewController(unwindSegue: UIStoryboardSegue) {
         
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
     }
     
 
---
+++ InterfaceTraining/InterfaceTraining/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  InterfaceTraining
+//
+//  Created by Yashim Greene on 8/2/17.
+//  Copyright © 2017 Yashim Greene. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
